14cf2d2a007e0eb3a454c4d2aff8d88d3a6ea4e5
Luke Herrington <lherrington100@gmail.com>
2017-06-11 15:08:37 -0700

    Updates

---
+++ _next/3e19290e-7238-49e2-baae-63e1ed165dd5/page/post/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/post', function() {
+            var comp = module.exports=webpackJsonp([5],{155:function(e,t,u){"use strict";function l(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var a=u(2),d=l(a),f=u(87),n=l(f),r=u(90),o=l(r),s=u(89),c=l(s);t.default=function(e){var t=e.url.query.post,u=c.default.findIndex(function(e){return e.slug===t});return d.default.createElement("div",null,d.default.createElement(n.default,{href:"/"},d.default.createElement("a",null,"Home")),d.default.createElement(o.default,{title:c.default[u].title,slug:c.default[u].slug,date:c.default[u].date,author:c.default[u].author,body:c.default[u].body,tags:c.default[u].tags}))}},356:function(e,t,u){e.exports=u(155)}},[356]);
+            return { page: comp.default }
+          })
+        
