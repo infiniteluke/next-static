ba5623bb40a7deacbef1f4bdb2ec94c2d22c4092
Luke Herrington <lherrington100@gmail.com>
2017-06-11 23:55:03 -0700

    Updates

--- _next/ca387f1f-df8c-46a6-b9af-c56b20bf131f/page/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/', function() {
-            var comp = module.exports=webpackJsonp([6],{154:function(e,t,u){"use strict";function l(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var a=u(2),n=l(a),o=u(90),r=l(o),d=u(89),f=l(d);t.default=function(){return n.default.createElement("div",null,n.default.createElement("h1",null,"My Blog"),f.default.map(function(e){return n.default.createElement(r.default,{key:e.slug,title:e.title,slug:e.slug,date:e.date,author:e.author,body:e.body,tags:e.tags})}))}},355:function(e,t,u){e.exports=u(154)}},[355]);
-            return { page: comp.default }
-          })
-        
--- _next/ca387f1f-df8c-46a6-b9af-c56b20bf131f/page/tag/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/tag', function() {
-            var comp = module.exports=webpackJsonp([4],{156:function(e,t,u){"use strict";function l(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var a=u(2),n=l(a),r=u(87),d=l(r),f=u(90),o=l(f),c=u(89),s=l(c);t.default=function(e){var t=e.url.query.tag;return n.default.createElement("div",null,n.default.createElement(d.default,{href:"/"},n.default.createElement("a",null,"Home")),n.default.createElement("h1",null,t),s.default.filter(function(e){return e.tags.includes(t)}).map(function(e){return n.default.createElement(o.default,{key:e.slug,title:e.title,slug:e.slug,date:e.date,author:e.author,body:e.body,tags:e.tags})}))}},357:function(e,t,u){e.exports=u(156)}},[357]);
-            return { page: comp.default }
-          })
-        
