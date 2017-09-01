5c391f0483090f1569dcabec44b4e75e0aff486e
Luke Herrington <lherrington100@gmail.com>
2017-09-01 08:38:04 -0700

    chore(git): Unignore posts file since to fix eslint.

---
+++ posts/index.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    title: 'My First Post',
+    slug: 'hello-world',
+    author: 'Luke H',
+    date: '2017-6-10 15:00 PDT',
+    tags: ['blog', 'first post'],
+    body:
+      "\n## Hello\nThis is an _example_ post that has some text that wraps. Simply use markdown to write your posts.\n\n## How to add code to a post\nMake sure to escape the backticks when writing code in you posts.\n\nHere's some code:\n```jsx\nconst a = <Post>{props.dope}</Post>;\n```\n\n## GFM Supported\nAnything that works in Github, works here:\n\n| First Header  | Second Header |\n| ------------- | ------------- |\n| Content Cell  | Content Cell  |\n| Content Cell  | Content Cell  |\n\n> This is a quote. This is a quote. This is a quote. This is a quote. This is a quote. This is a quote. This is a quote. This is a quote.\n",
+  },
+  {
+    title: 'My Next Post',
+    slug: 'next-post',
+    author: 'Luke H',
+    date: '2017-6-10 15:26 PDT',
+    tags: ['blog', 'dad jokes'],
+    body:
+      "\n## Puns\n_Next_ post. Get it? _Next_? I'll let myself out...\n\n```bash\necho $PATH\n```\n",
+  },
+];
